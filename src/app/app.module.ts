a737feb16619709b2b5ccb934e003761ef90e881
Mateusz Orlicki <mateuszorlicki@o2.pl>
2020-02-27 15:11:57 +0100

    environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+import 'firebase';
 import { environment } from '../environments/environment';
 import { AuthModule } from './auth/auth.module';
 import { MainModule } from './main/main.module';
