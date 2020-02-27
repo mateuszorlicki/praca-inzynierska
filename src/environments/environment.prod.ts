184c32d88213c40087885af131fd2401633ebede
Mateusz Orlicki <mateuszorlicki@o2.pl>
2020-02-27 15:04:46 +0100

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyANiE_iemjaAqerukNXPUERyNxeKpKf-GM",
+    authDomain: "gym2020.firebaseapp.com",
+    databaseURL: "https://gym2020.firebaseio.com",
+    projectId: "gym2020",
+    storageBucket: "gym2020.appspot.com",
+    messagingSenderId: "420405131543"
+  }
 };
